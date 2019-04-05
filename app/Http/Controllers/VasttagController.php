42c94edb3ed41e8689db970dee55ce3a12b7cb4e
Joris Wessels <joris@e-heroes.nl>
2019-04-05 10:02:55 +0200

    Added Migrations, Controllers and Models

---
+++ app/Http/Controllers/VasttagController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Vasttag;
+use Illuminate\Http\Request;
+
+class VasttagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Vasttag  $vasttag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vasttag $vasttag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Vasttag  $vasttag
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vasttag $vasttag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Vasttag  $vasttag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vasttag $vasttag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Vasttag  $vasttag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vasttag $vasttag)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_04_05_075046_create_vasttags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVasttagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vasttags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('provider_name');
+            $table->string('url');
+            $table->integer('zone_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vasttags');
+    }
+}
